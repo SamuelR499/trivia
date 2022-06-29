f567d962262045eff2c39c3234e168f2da43f78f
Lucas Otoni <lotoni.vivo@gmail.com>
2022-06-29 11:53:32 -0300

    req 7 quase completo

---
+++ src/reducers/game.js
@@ -0,0 +1,16 @@
+// import { EXPORT_QUESTIONS } from '../actions';
+
+const initialState = {
+  api: {},
+};
+
+const game = (state = initialState, action) => {
+  switch (action.type) {
+  default:
+    return {
+      ...state,
+    };
+  }
+};
+
+export default game;
