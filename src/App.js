2b925e6f7def80f8df6f3f440d8f3a11a3eda26b
Samuelr499 <samuelribeiro2@hotmail.com>
2022-12-04 13:25:28 -0300

    style: estilo da pagina de login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Login from './pages/Login';
+import Login from './pages/login/Login';
 import Game from './pages/Game';
 import './App.css';
 import Setting from './pages/Setting';
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import md5 from 'crypto-js/md5';
-import { getToken, submitLoginForm } from '../actions';
+import { getToken, submitLoginForm } from '../../actions';
+import './Login.css';
+import imgLogo from '../../images/triviaLogo.png';
 
 class Login extends React.Component {
   state = {
@@ -18,7 +20,7 @@ class Login extends React.Component {
     const gravatarEmail = md5(email).toString();
     submitEmailAndNames({ gravatarEmail, nome });
     history.push('/jogo');
-  }
+  };
 
   handleChange = ({ target }) => {
     const { name, value } = target;
@@ -38,39 +40,51 @@ class Login extends React.Component {
     const { history } = this.props;
     const { email, nome, lockButton } = this.state;
     return (
-      <div>
-        <input
-          data-testid="input-player-name"
-          name="nome"
-          type="text"
-          placeholder="Nome"
-          value={ nome }
-          onChange={ this.handleChange }
-        />
-        <input
-          data-testid="input-gravatar-email"
-          name="email"
-          type="email"
-          placeholder="Email"
-          value={ email }
-          onChange={ this.handleChange }
-        />
-        <button
-          type="button"
-          data-testid="btn-play"
-          disabled={ lockButton }
-          onClick={ this.handleClick }
-        >
-          Play
-        </button>
-        <button
-          type="button"
-          data-testid="btn-settings"
-          onClick={ () => history.push('/settings') }
-        >
-          Settings
-        </button>
-      </div>
+      <section className="area-login">
+        <div className="login">
+          <div className="logo-area">
+            <img src={ imgLogo } alt="" />
+            <h1>Trivia</h1>
+          </div>
+          <form>
+            <input
+              data-testid="input-player-name"
+              name="nome"
+              type="text"
+              placeholder="nome de usuario"
+              value={ nome }
+              onChange={ this.handleChange }
+            />
+            <input
+              data-testid="input-gravatar-email"
+              name="email"
+              type="email"
+              placeholder="email"
+              value={ email }
+              onChange={ this.handleChange }
+            />
+            <div className="btn-area">
+              <button
+                className="button-form"
+                type="button"
+                data-testid="btn-play"
+                disabled={ lockButton }
+                onClick={ this.handleClick }
+              >
+                Play
+              </button>
+              <button
+                className="button-form"
+                type="button"
+                data-testid="btn-settings"
+                onClick={ () => history.push('/settings') }
+              >
+                Settings
+              </button>
+            </div>
+          </form>
+        </div>
+      </section>
     );
   }
 }
